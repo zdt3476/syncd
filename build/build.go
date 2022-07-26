cff303c51dc557b7acf6306f6a7bf8ac469e9d24
Zhangdaotian <zhangdaotian@kosmoframe.com>
2022-07-26 19:01:06 +0800

    update

--- build/build.go
+++ build/build.go
@@ -73,13 +73,10 @@ func (b *Build) initBuildTask() {
 	cmds = append(cmds, []string{
 		"echo \"Now is\" `date`",
 		"echo \"Run user is\" `whoami`",
-		fmt.Sprintf("cd %s", b.local),
-		"echo \"Commit message is\" `git log --pretty=oneline --abbrev-commit | head -n 1`",
-		"echo \"Current directory is\" `pwd`",
-		// fmt.Sprintf("rm -f %s", b.packFile),
+		fmt.Sprintf("cd %s && echo \"Commit message is\" `git log --pretty=oneline --abbrev-commit | head -n 1` && echo \"Current directory is\" `pwd`", b.local),
 		fmt.Sprintf("cd %s && /bin/bash -c %s", b.local, b.scriptFile),
-		// fmt.Sprintf("rm -f %s", b.scriptFile),
-		// fmt.Sprintf("rm -fr %s", b.local),
+		fmt.Sprintf("rm -f %s", b.scriptFile),
+		fmt.Sprintf("rm -fr %s", b.local),
 		"echo \"Compile completed\" `date`",
 	}...)
 	b.task = command.NewTask(cmds, COMMAND_TIMEOUT)
