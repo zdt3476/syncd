158c0b8a613744791e118283fca83a3ce6945c06
Zhangdaotian <zhangdaotian@kosmoframe.com>
2022-07-26 17:21:10 +0800

    编译时跳转到项目根目录

--- build/build.go
+++ build/build.go
@@ -73,8 +73,9 @@ func (b *Build) initBuildTask() {
 	cmds = append(cmds, []string{
 		"echo \"Now is\" `date`",
 		"echo \"Run user is\" `whoami`",
-		"git log --pretty=oneline --abbrev-commit | head -n 1",
-		"echo \"Show current directory\" `pwd`",
+		"echo \"Commit message is\" `git log --pretty=oneline --abbrev-commit | head -n 1`",
+		fmt.Sprintf("cd %s", b.local),
+		"echo \"Current directory is\" `pwd`",
 		// fmt.Sprintf("rm -f %s", b.packFile),
 		fmt.Sprintf("/bin/bash -c %s", b.scriptFile),
 		fmt.Sprintf("rm -f %s", b.scriptFile),
